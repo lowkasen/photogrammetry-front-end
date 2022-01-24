94d11b446d559bb45e8d129144399588f0bdf70a
lowkasen <lowkasen@gmail.com>
2022-01-24 10:03:34 +0800

    updated uploadimages api

--- pages/api/uploadimages.ts
+++ pages/api/uploadimages.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { randomUUID } from 'crypto';
+import Error from 'next/error';
 
 export default async function handler(
     req: NextApiRequest,
@@ -10,15 +12,21 @@ export default async function handler(
         const s3Client = new S3Client({ region: REGION });
         const params = {
             Bucket: "photogrammetry-capstone-bucket-1", // The name of the bucket. For example, 'sample_bucket_101'.
-            Key: "hello/sample_upload.txt", // The name of the object. For example, 'sample_upload.txt'.
+            Key: randomUUID() + "/sample_upload.txt", // The name of the object. For example, 'sample_upload.txt'.
             Body: "Hello world!", // The content of the object. For example, 'Hello world!".
         };
         const command = new PutObjectCommand(params);
         const results = await s3Client.send(command);
-        console.log(results);
+        
+        res.status(200).json({ 
+            params: params
+        })
+
     } catch(err) {
         console.error(err);
+        res.status(500).json({
+            error: 'uploadimages api error',
+            //message: err
+        })
     }
-    
-    res.status(200).json({ name: 'John Doe' })
 }
