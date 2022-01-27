3ceacdf4b1b865418608fac91bfb96ff08ad1ab7
Ka Sen Low <lowkasen@gmail.com>
2022-01-27 18:22:11 +0800

    adding logging to getuuid

--- pages/api/getuuid.ts
+++ pages/api/getuuid.ts
@@ -1,15 +1,20 @@
-import { randomUUID } from 'crypto'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { randomUUID } from "crypto";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        const uuid = randomUUID();
-        res.status(200).json({uuid:uuid});
-    } catch (err) {
-        const errorJSON = JSON.stringify(err, Object.getOwnPropertyNames(err), '\t');
-        res.status(500).json(errorJSON);
-    }
+  try {
+    const uuid = randomUUID();
+    console.log(uuid);
+    res.status(200).json({ uuid: uuid });
+  } catch (err) {
+    const errorJSON = JSON.stringify(
+      err,
+      Object.getOwnPropertyNames(err),
+      "\t"
+    );
+    res.status(500).json(errorJSON);
+  }
 }
